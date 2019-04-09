1108c93dc12450e14e5b6ff39bd48f81057eb4aa
Megha Walia <mwalia2@sapient.com>
2019-04-09 10:10:41 +0000

    Code committed after Initial Code set Up

---
+++ TrackMyDay/TrackMyDay/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrackMyDay.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TrackMyDay/TrackMyDay/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrackMyDay.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
